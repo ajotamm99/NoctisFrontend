1c80964a0ad321dcfbede1f2a5e4a45e7b0c3a55
ajotamm99 <72545812+ajotamm99@users.noreply.github.com>
2024-01-17 23:12:55 +0100

    TFM Demostracion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'black', width: '100vw', height: '100vh' }}>
-      <h1 style={{ fontSize: '4rem', color: 'rgba(255, 255, 255, 1)' }}>Noctis</h1>
+      <h1 style={{ fontSize: '4rem', color: 'rgba(255, 255, 255, 1)' }}>Noctis 2</h1>
     </main>
   )
 }
